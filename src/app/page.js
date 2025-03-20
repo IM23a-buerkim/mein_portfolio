21a07f0b3676266980902e2407cc8737577a1a7e
IM23a-buerkim <buerkim@bzz.ch>
2025-03-20 08:13:38 +0100

    test 2

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import Layout from './layout';
-import './styles.css';
+import './mainstyle.css';
+
 
 const Page = () => {
-  return (
-      <div className="content">
-        <h2>main</h2>
-      </div>
-  );
+    return (
+        <div className="content">
+            <h2>Projekte</h2>
+            <h2>Über mich</h2>
+            <h2>Skills</h2>
+        </div>
+    );
 };
 
 export default Page;

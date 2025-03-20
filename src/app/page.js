f96b6e27f9bd6a0898825dbf31a262d8c13507a6
IM23a-buerkim <buerkim@bzz.ch>
2025-03-20 09:29:08 +0100

    skills teil 1

--- src/app/page.js
+++ src/app/page.js
@@ -6,9 +6,33 @@ import './mainstyle.css';
 const Page = () => {
     return (
         <div className="content">
-            <h2>Projekte</h2>
-            <h2>Über mich</h2>
-            <h2>Skills</h2>
+            <div id="projekte">
+                <h2>Projekte</h2>
+            </div>
+            <div id="ueber">
+                <h2>Über mich</h2>
+            </div>
+            <div id="skills">
+                <div id="s_titel">Skills</div>
+                <div id="s_list">
+                    <div className="s_l_element">
+                        <div className="s_l_icon" style={{backgroundImage: "url('/dslr-camera.png')"}}></div>
+                        <div className="s_l_titel">Fotografie</div>
+                    </div>
+                    <div className="s_l_element">
+                        <div className="s_l_icon" style={{backgroundImage: "url('/rowing-boat.png')"}}></div>
+                        <div className="s_l_titel">Rudern</div>
+                    </div>
+                    <div className="s_l_element">
+                        <div className="s_l_icon" style={{backgroundImage: "url('/bicycle.png')"}}></div>
+                        <div className="s_l_titel">Fahrrad<br/>fahren</div>
+                    </div>
+                    <div className="s_l_element">
+                        <div className="s_l_icon" style={{backgroundImage: "url('/data.png')"}}></div>
+                        <div className="s_l_titel">Programmieren</div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };

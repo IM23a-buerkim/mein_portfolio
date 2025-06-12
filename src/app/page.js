4f9123f24a53d3b701f74771902d6ce721582867
IM23a-buerkim <buerkim@bzz.ch>
2025-06-12 09:56:28 +0200

    sehr viel neues

--- src/app/page.js
+++ src/app/page.js
@@ -1,20 +1,22 @@
-import React from 'react';
+"use client";
+import React, {useState} from 'react';
 import './variables.css';
 import './styles.css';
 import './mainstyle.css';
 import './projekte_style.css';
-import AgeComponent from'./age.js';
+import AgeComponent from './age.js';
 import Project from './projekte';
 
-
 const Page = () => {
+    const [showProgrammierSkills, setShowProgrammierSkills] = useState(true);
+
     return (
         <div className="content">
             <Project/>
             <div id="ueber_abstand"></div>
             <div id="ueber">
                 <div id="ueber_links">
-                    <img id="ueber_links_foto" src="/Manuel_Portfolio.jpg" alt="Mein Foto"></img>
+                    <img id="ueber_links_foto" src="/Manuel_Portfolio.jpg" alt="Mein Foto"/>
                 </div>
                 <div id="ueber_rechts">
                     <div id="ueber_rechts_titel">Über mich</div>
@@ -41,26 +43,47 @@ const Page = () => {
                     </div>
                 </div>
             </div>
+
             <div id="skills">
-                <div id="s_titel">Skills</div>
-                <div id="s_list">
-                    <div className="s_l_element">
-                        <div className="s_l_icon" style={{backgroundImage: "url('/dslr-camera.png')"}}></div>
-                        <div className="s_l_titel">Fotografie</div>
-                    </div>
-                    <div className="s_l_element">
-                        <div className="s_l_icon" style={{backgroundImage: "url('/rowing-boat.png')"}}></div>
-                        <div className="s_l_titel">Rudern</div>
-                    </div>
-                    <div className="s_l_element">
-                        <div className="s_l_icon" style={{backgroundImage: "url('/bicycle.png')"}}></div>
-                        <div className="s_l_titel">Fahrrad<br/>fahren</div>
-                    </div>
-                    <div className="s_l_element">
-                        <div className="s_l_icon" style={{backgroundImage: "url('/data.png')"}}></div>
-                        <div className="s_l_titel">Programmieren</div>
+                <div id="skills_switch_line">
+                    <div id="skills_switch_field">
+                        <div
+                            id="skills_switch_Andere"
+                            className={showProgrammierSkills ? '' : 'active'}
+                            onClick={() => setShowProgrammierSkills(false)}
+                        >Andere</div>
+                        <div
+                            id="skills_switch_Programmieren"
+                            className={showProgrammierSkills ? 'active' : ''}
+                            onClick={() => setShowProgrammierSkills(true)}
+                        >Programmieren</div>
                     </div>
                 </div>
+                <div id="s_titel">Skills</div>
+
+                {showProgrammierSkills ? (
+                    <div id="s_list_programmieren" style={{display: 'flex'}}>
+                        <div className="s_l_element">
+                            <div className="s_l_icon" style={{backgroundImage: "url('/data.png')"}}></div>
+                            <div className="s_l_titel">Programmieren</div>
+                        </div>
+                    </div>
+                ) : (
+                    <div id="s_list_andere" style={{display: 'flex'}}>
+                        <div className="s_l_element">
+                            <div className="s_l_icon" style={{backgroundImage: "url('/dslr-camera.png')"}}></div>
+                            <div className="s_l_titel">Fotografie</div>
+                        </div>
+                        <div className="s_l_element">
+                            <div className="s_l_icon" style={{backgroundImage: "url('/rowing-boat.png')"}}></div>
+                            <div className="s_l_titel">Rudern</div>
+                        </div>
+                        <div className="s_l_element">
+                            <div className="s_l_icon" style={{backgroundImage: "url('/bicycle.png')"}}></div>
+                            <div className="s_l_titel">Fahrrad<br/>fahren</div>
+                        </div>
+                    </div>
+                )}
             </div>
         </div>
     );
--- src/app/projekte.js
+++ src/app/projekte.js
@@ -72,51 +72,53 @@ const Project = () => {
             <div id="projekt_strich"></div>
             <div id="projekt_container" ref={containerRef}>
                 <div className="projekt_element">
-                    <div className="projekt_icon" style={{backgroundImage: "url('/data.png')"}}></div>
+                    <div className="projekt_icon" style={{backgroundImage: "url('/SCK_Try1.png')"}}></div>
                     <div className="projekt_inhalt">
-                        <div className="projekt_titel">Projekt 1</div>
-                        <div className="projekt_text">Dies ist ein Beispieltext für Projekt 1.</div>
+                        <div className="projekt_titel">SCK_Try1</div>
+                        <div className="projekt_text">Bei diesem Projekt habe ich versucht die Webseite meines Ruderclubs
+                        zu verbessern.</div>
                         <div className="projekt_inhalt2">
-                            <div className="projekt_sprache">Java</div>
+                            <div className="projekt_sprache">HTML</div>
+                            <div className="projekt_sprache">CSS</div>
+                            <div className="projekt_sprache">JavaScript</div>
                         </div>
                     </div>
                 </div>
                 <div className="projekt_element">
-                    <div className="projekt_icon" style={{backgroundImage: "url('/data.png')"}}></div>
+                    <div className="projekt_icon" style={{backgroundImage: "url('/Geschichte-der-Schweizer-Uhrenindustrie.png')"}}></div>
                     <div className="projekt_inhalt">
-                        <div className="projekt_titel">Projekt 2</div>
-                        <div className="projekt_text">Dies ist ein Beispieltext für Projekt 1. Dies ist ein Beispieltext
-                            für
-                            Projekt 1. Dies ist ein Beispieltext für Projekt 1.
+                        <div className="projekt_titel">Geschichte-der-Schweizer-Uhrenindustrie</div>
+                        <div className="projekt_text">Dieses Projekt habe ich mit Klassenkameraden gemacht. Wir haben
+                            die Geschichte der Schweizer Uhrenindustrie auf einer Webseite dargestellt.
                         </div>
                         <div className="projekt_inhalt2">
-                            <div className="projekt_sprache">Java</div>
-                            <div className="projekt_sprache">Java</div>
-                            <div className="projekt_sprache">Java</div>
-                            <div className="projekt_sprache">Java</div>
-                            <div className="projekt_sprache">Java</div>
-                            <div className="projekt_sprache">Java</div>
+                            <div className="projekt_sprache">HTML</div>
+                            <div className="projekt_sprache">CSS</div>
+                            <div className="projekt_sprache">JavaScript</div>
                         </div>
                     </div>
                 </div>
                 <div className="projekt_element">
-                    <div className="projekt_icon" style={{backgroundImage: "url('/data.png')"}}></div>
+                    <div className="projekt_icon" style={{backgroundImage: "url('/Schiffe-versenken.png')"}}></div>
                     <div className="projekt_inhalt">
-                        <div className="projekt_titel">Projekt 3</div>
-                        <div className="projekt_text">Dies ist ein Beispieltext für Projekt 1.</div>
+                        <div className="projekt_titel">Schiffe-versenken</div>
+                        <div className="projekt_text">Hier habe ich vor langer Zeit einmal ein Schiffe versenken Spiel
+                            gemacht, nur mit Python.</div>
                         <div className="projekt_inhalt2">
-                            <div className="projekt_sprache">Java</div>
+                            <div className="projekt_sprache">Python</div>
                         </div>
                     </div>
                 </div>
                 <div id="projekt_element_wrapper_last">
                     <div className="projekt_element">
-                        <div className="projekt_icon" style={{backgroundImage: "url('/data.png')"}}></div>
+                        <div className="projekt_icon" style={{backgroundImage: "url('/mein_portfolio.png')"}}></div>
                         <div className="projekt_inhalt">
-                            <div className="projekt_titel">Projekt 4</div>
-                            <div className="projekt_text">Dies ist ein Beispieltext für Projekt 1.</div>
+                            <div className="projekt_titel">mein_portfolio</div>
+                            <div className="projekt_text">Das ist das Gitrepository von dieser Webseite.</div>
                             <div className="projekt_inhalt2">
-                                <div className="projekt_sprache">Java</div>
+                                <div className="projekt_sprache">JavaScript</div>
+                                <div className="projekt_sprache">HTML</div>
+                                <div className="projekt_sprache">CSS</div>
                             </div>
                         </div>
                     </div>
